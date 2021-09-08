4e0dc26c14cbd5508e0a3004c282ae684c4b8a79
Olesya Yakun <yakunicheva.olesya@yandex.ru>
2021-09-08 20:15:42 +0300

    delete all

--- Isu/Lab0/Models/Student.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Lab0.Models
-{
-    public class Student
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public Group Group { get; set; }
-    }
-}
--- Isu/Lab0/Tests/IsuServiceException.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace Lab0.Tests
-{
-    public class IsuServiceException : Exception
-    {
-        public IsuServiceException(string message) : base(message) { }
-    }
-}
