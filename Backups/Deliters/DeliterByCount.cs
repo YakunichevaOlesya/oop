cd5007e411f388930ad18b6bb0372cda167f2a97
Olesya Yakun <yakunicheva.olesya@yandex.ru>
2021-11-26 17:22:25 +0300

    finish lab3

---
+++ Backups/Deliters/DeliterByCount.cs
@@ -0,0 +1,24 @@
+﻿using Lab3.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3.Deliters
+{
+    public class DeliterByCount : IDelitable<int>
+    {
+        public List<int> DeleteByCriterion(int n, List<RestorePoint> points)
+        {
+            List<RestorePoint> pointsToSave = new List<RestorePoint>(points);
+            List<int> pointsToDelete = new List<int>();
+            while (pointsToSave.Count > n) {
+                pointsToDelete.Add(pointsToSave.First().ID);
+                pointsToSave.RemoveAt(0);
+            }
+
+            return pointsToDelete;
+        }
+    }
+}
---
+++ Backups/HelpClass.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3
+{
+    public static class HelpClass
+    {
+        // Help methods
+        public static string MySubString(string source, int start, int end)
+        {
+            string result = "";
+            for (int i = start; i < end; i++)
+            {
+                result += source[i];
+            }
+            return result;
+        }
+    }
+}
